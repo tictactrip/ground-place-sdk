cdefd782e1dbff4834796ebe7d5c5eb73294d299
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2020-10-26 16:57:08 +0100

    chore(jest.config.js): file created

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+};
