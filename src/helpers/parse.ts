8293c4f028cdc65d73ad50840f2561ce127804e9
Moussa Iskounene <moussa.iskounene@gmail.com>
2020-12-29 10:06:38 +0100

    update(parse): Add returns to parse method

--- src/helpers/parse.ts
+++ src/helpers/parse.ts
@@ -3,6 +3,7 @@ import { GroundPlaceServiced, PlaceCreated, GroundPlace } from '../../src/types'
 /**
  * @description Parse the place created to format it with the GroundPlaces structure.
  * @param {PlaceCreated} placeCreated - The place created from the `@tictactrip/gp-uid` package.
+ * @returns {GroundPlace}
  */
 const parsePlaceCreated = (placeCreated: PlaceCreated): GroundPlace => ({
   gpuid: placeCreated.id,
