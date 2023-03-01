ee3d93c5111596ed39b6d7a13fcb8e42ca5151db
Alexandre <alex.forest1026@gmail.com>
2023-03-01 15:10:00 +0100

    fix: updated tests

--- __tests__/groundplaces/createStopCluster.spec.ts
+++ __tests__/groundplaces/createStopCluster.spec.ts
@@ -28,6 +28,7 @@ describe('#createStopCluster', () => {
         warning: false,
         country_code: 'fr',
         is_latest: true,
+        is_high_speed: true,
         name: 'Strasbourg, Grand-Est, France',
         longitude: 7.74815,
         latitude: 48.583,
